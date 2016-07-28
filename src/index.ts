19e78adf055e5a0ca7630cdee9da189b32e7e5da
Zak Henry <zak.henry@gmail.com>
2016-07-28 15:08:01 +0100

    refactor(classes):Refactor to class based for cleaner state persistence

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,49 @@
 /**
  * Main public method to convert a number into the english string representation
- * @param number
+ * @param input
  * @returns {string}
  */
-export function uintToWords(number: number): string {
+export function uintToWords(input: number): string {
 
-  if (!Number.isInteger(number)){
-    throw new TypeError('Input must be an integer');
+  return new Converter(input).convert();
+}
+
+class Converter {
+
+  private readonly input: number;
+
+  constructor(inputNumber: number) {
+    this.input = inputNumber;
+    this.validate();
   }
 
-  if (number < 0){
-    throw new RangeError('Input must be greater or equal to zero');
+  /**
+   * Validate the input, throws instanceof Error if invalid
+   * @returns {boolean}
+   */
+  protected validate(): boolean {
+
+    if (!Number.isInteger(this.input)) {
+      throw new TypeError('Input must be an integer');
+    }
+
+    if (this.input < 0) {
+      throw new RangeError('Input must be greater or equal to zero');
+    }
+
+    if (this.input > 999999999) {
+      throw new RangeError('Input must be less than one billion');
+    }
+
+    return true;
   }
 
-  if (number > 999999999){
-    throw new RangeError('Input must be less than one billion');
+  /**
+   * Return the finally converted value
+   * @returns {string}
+   */
+  public convert(): string {
+    return '';
   }
 
-  return '';
 }
