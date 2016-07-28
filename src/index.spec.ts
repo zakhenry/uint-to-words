22886b29202986eccf85f5e087bb6e92026a729b
Zak Henry <zak.henry@gmail.com>
2016-07-28 14:44:46 +0100

    feat(validation): Add type checks to verify input is in range and correct type

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,13 +1,37 @@
-import { foo } from './index';
 import * as chai from 'chai';
+import { uintToWords } from './index';
 
 const expect = chai.expect;
 
-describe('a test suite', () => {
+describe('library functionality', () => {
 
-  it('foo should be bar', () => {
+  it('converts a number into a string', () => {
 
-    expect(foo).to.eq('bar');
+    expect(uintToWords(10)).to.be.a('string');
+
+  });
+
+  it('throws error when input is not a string', () => {
+
+    const test = () => uintToWords('not-a-number' as any);
+
+    expect(test).to.throw(TypeError, 'Input must be an integer');
+
+  });
+
+  it('throws error when input not a positive integer', () => {
+
+    const test = () => uintToWords(-10);
+
+    expect(test).to.throw(RangeError, 'Input must be greater or equal to zero');
+
+  });
+
+  it('throws error when input is too large (>1bn)', () => {
+
+    const test = () => uintToWords(1000000000);
+
+    expect(test).to.throw(RangeError, 'Input must be less than one billion');
 
   });
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export const foo = 'bar';
+/**
+ * Main public method to convert a number into the english string representation
+ * @param number
+ * @returns {string}
+ */
+export function uintToWords(number: number): string {
+
+  if (!Number.isInteger(number)){
+    throw new TypeError('Input must be an integer');
+  }
+
+  if (number < 0){
+    throw new RangeError('Input must be greater or equal to zero');
+  }
+
+  if (number > 999999999){
+    throw new RangeError('Input must be less than one billion');
+  }
+
+  return '';
+}
